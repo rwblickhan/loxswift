0e3c86047777aed923a94ab970af04c18e32c74f
Russell Blickhan <rwblickhan@gmail.com>
2022-06-09 23:02:56 -0700

    Implement chapter 1

--- Package.swift
+++ Package.swift
@@ -14,9 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "loxswift",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "loxswiftTests",
-            dependencies: ["loxswift"]),
+            dependencies: ["loxswift"]
+        ),
     ]
 )
--- Sources/loxswift/main.swift
+++ Sources/loxswift/main.swift
@@ -1,1 +1,8 @@
-print("Hello, world!")
+import Foundation
+
+let chunk = Chunk()
+chunk.write(Opcode.return.rawValue, line: 123)
+let constant = chunk.addConstant(1.2)
+chunk.write(Opcode.constant.rawValue, line: 123)
+chunk.write(UInt8(constant), line: 123)
+disassemble(chunk, name: "test chunk")
